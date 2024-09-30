4ddbd7f1dbcbd7369ce5ecc7af0c86f243448595
Renzo E. Guarise <renzo.guarise@mercadolibre.com>
2024-09-29 21:07:49 -0300

    Release/0.0.1 (#6)
    * Part 1 (#2)
    
    * Fix dependencies
    
    * Fix dependencies
    
    * Fix notebook
    
    * Add notebook
    
    * Add train and save model
    
    * Add part 2 (#3)
    
    * Part 3 (#5)
    
    * Add part 3
    
    * Add docs
    
    * Update model

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,21 @@
-# syntax=docker/dockerfile:1.2
-FROM python:latest
-# put you docker configuration here
+FROM python:3.11-slim
+
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements file into the container
+COPY requirements.txt /app/
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code into the container
+COPY challenge/ /app/
+
+
+# Expose the port your app runs on
+EXPOSE 8080
+
+# Command to run your FastAPI app using Uvicorn
+CMD ["uvicorn", "api:app", "--host", "0.0.0.0", "--port", "8080"]
--- challenge/model.py
+++ challenge/model.py
@@ -1,6 +1,32 @@
 import pandas as pd
-
+import pathlib
+import pickle
+import numpy as np
 from typing import Tuple, Union, List
+from datetime import datetime
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import  OneHotEncoder
+
+
+TOP_10_FEATURES = [
+    "OPERA_Latin American Wings", 
+    "MES_7",
+    "MES_10",
+    "OPERA_Grupo LATAM",
+    "MES_12",
+    "TIPOVUELO_I",
+    "MES_4",
+    "MES_11",
+    "OPERA_Sky Airline",
+    "OPERA_Copa Air"
+]
+
+DELAY_THRESHOLD_IN_MINUTES: int = 15
+
+THRESHOLD_PREDICT: float = 0.5
+
+FEATURE_COLUMNS =['OPERA', 'TIPOVUELO', 'MES']
 
 class DelayModel:
 
@@ -8,12 +34,25 @@ class DelayModel:
         self
     ):
         self._model = None # Model should be saved in this attribute.
+        self.preprocess_pipeline = OneHotEncoder(sparse=False)
+
+    def _get_min_diff(self, row: pd.Series) -> float:
+        """
+        Calculate the minute difference between two date columns.
+
+        Args:
+            row (pd.Series): Row of the dataset.
+        """
+        fecha_o = datetime.strptime(row['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+        fecha_i = datetime.strptime(row['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+        min_diff = ((fecha_o - fecha_i).total_seconds()) / 60
+        return min_diff
 
     def preprocess(
         self,
         data: pd.DataFrame,
         target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
         """
         Prepare raw data for training or predict.
 
@@ -26,7 +65,24 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+        
+        
+        # Generate 'delay' column based on a threshold of 15 minutes
+        
+        # Feature encoding
+        
+        
+        if target_column:
+            data['min_diff'] = data.apply(self._get_min_diff, axis=1)
+            data['delay'] = np.where(data['min_diff'] > DELAY_THRESHOLD_IN_MINUTES, 1, 0)
+            features_encoded = self.preprocess_pipeline.fit_transform(data[FEATURE_COLUMNS])
+            column_names = self.preprocess_pipeline.get_feature_names_out(input_features=FEATURE_COLUMNS)
+            features = pd.DataFrame(features_encoded, columns=column_names)[TOP_10_FEATURES]
+            return features, data[[target_column]]
+        else:
+            features_encoded = self.preprocess_pipeline.transform(data[FEATURE_COLUMNS])
+            features = pd.DataFrame(features_encoded, columns=self.preprocess_pipeline.get_feature_names_out())[TOP_10_FEATURES]
+            return features
 
     def fit(
         self,
@@ -40,7 +96,14 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
-        return
+        # Balance Class Weights
+        target = target.to_numpy()
+        n_y0 = len(target[target == 0])
+        n_y1 = len(target[target == 1])
+        
+        # Train Logistic Regression with balanced class weights
+        self._model = LogisticRegression(class_weight={1: n_y0 / len(target), 0: n_y1 / len(target)})
+        self._model.fit(features, target)
 
     def predict(
         self,
@@ -55,4 +118,20 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        return
+        return [1 if y_pred > THRESHOLD_PREDICT else 0 for y_pred in self._model.predict(features)]
+
+    def evaluate(self, features: pd.DataFrame, target: pd.Series) -> None:
+        """Evaluate the trained model."""
+        y_preds = self._model.predict(features)
+        print(confusion_matrix(target, y_preds))
+        print(classification_report(target, y_preds))
+    
+    def save(self, path: str) -> None:
+        """
+        Method to save train model
+
+        Args:
+            path (str): path where it is going to be save
+        """
+        with open(path, "wb") as f_out:
+            pickle.dump(self, f_out)
